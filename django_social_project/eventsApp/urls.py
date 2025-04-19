8d7b77eec9c993cee334ebbb9cf803e70b81534f
Gabriel-Sanchez <gabriel.g.sanchez.c@gmail.com>
2025-04-19 16:16:10 -0500

    calendar api

--- django_social_project/eventsApp/urls.py
+++ django_social_project/eventsApp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.events, name='events'),  # Ruta principal
-    path('api/calendar-events/', views.get_calendar_events, name='calendar_events'),
+    path('calendar-events/', views.get_calendar_events, name='calendar_events'),  # API para eventos del calendario
 ]
